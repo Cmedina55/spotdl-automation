54bdea02b5dfd611e3c10f0e315e03412e370b4b
Cmedina55 <cmedina@mediosenred.tv>
2024-12-19 21:10:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import os
-import subprocess
-
-def download_spotify_track(url):
-    try:
-        result = subprocess.run(['spotdl', url], check=True, capture_output=True, text=True)
-        print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e.stderr}")
-
-if __name__ == "__main__":
-    track_url = "https://open.spotify.com/intl-es/track/7CkJfQByYyFdu9jRRXk858"
-    download_spotify_track(track_url)
+spotdl https://open.spotify.com/intl-es/track/1Rj5fXGOrfFpynoO19Xtar?si=9a5c8b13e3584c97
